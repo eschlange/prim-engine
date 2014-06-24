4000486dc756b39b6fee61c44b24d427c3435481
Eric Schlange <eric.schlange@northwestern.edu>
2014-06-24 10:50:40 -0500

    Moved routes to specify api version number. Refactored some naming of models.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 PrimEngine::Engine.routes.draw do
-  resources :api_participants
-  resources :participants
+
+  namespace :api, defaults: { format: 'json' }, path: '' do
+    namespace :v1 do
+      resources :api_participants
+      resources :participants
+    end
+  end
 
 end
